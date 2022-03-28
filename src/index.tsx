6fd63be0bb149227c3b9d4f4d349074ff8b9cfdd
Jean-Michel Laforge <thejmguitars@gmail.com>
2022-03-28 14:01:17 -0400

    updatedredirectUri

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,7 @@ import { Auth0Provider } from "@auth0/auth0-react";
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import { Auth0 } from "./auth";
+import isDev from "./isDev";
 
 const client = new ApolloClient({
   uri: "https://www.dnd5eapi.co/graphql",
@@ -16,7 +17,8 @@ ReactDOM.render(
   <Auth0Provider
     domain={Auth0.DOMAIN}
     clientId={Auth0.CLIENTID}
-    redirectUri={window.location.origin}
+    redirectUri={"https://jean-monsieur.github.io/dnd-graphql/"}
+    // redirectUri={isDev() ? window.location.origin : "https://jean-monsieur.github.io/dnd-graphql/"}
   >
     <BrowserRouter>
       <ApolloProvider client={client}>
