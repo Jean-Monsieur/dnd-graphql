caa8de4e0f93f38a84f76f55420f5e0a22b71136
Jean-Michel Laforge <thejmguitars@gmail.com>
2022-03-31 18:34:39 -0400

    feat(Equipment): :card_file_box: added equipement query

--- src/components/equipmentQuery.ts
+++ src/components/equipmentQuery.ts
@@ -1,32 +1,86 @@
 import { gql } from "@apollo/client";
 
-export const QUERY_EQUIPMENT = gql`
-  query getEquipments {
-    equipments(limit: 1000) {
-      __typename
-      index
-      weight
-      name
-      quantity
-      capacity
-      category_range
-      gear_category {
+export const QUERY_EQUIPMENTS = gql`
+query getEquipment($filter: FilterFindOneEquipmentInput) {
+  equipment(filter: $filter) {
+    index
+    name
+    armor_category
+    armor_class {
+      base
+      dex_bonus
+      max_bonus
+    }
+    capacity
+    category_range
+    contents {
+      item {
+        index
         name
+        url
       }
-      desc
-      special
-      cost {
-        quantity
-        unit
-        __typename
-      }
-      damage {
-        damage_dice
-        damage_type {
-          name
-          __typename
-        }
+      quantity
+    }
+    cost {
+      quantity
+      unit
+    }
+    damage {
+      damage_dice
+      damage_type {
+        index
+        name
+        url
       }
     }
+    desc
+    equipment_category {
+      index
+      name
+      url
+    }
+    gear_category {
+      index
+      name
+      url
+    }
+
+    properties {
+      desc
+      index
+      name
+      url
+    }
+    quantity
+    range {
+      long
+      normal
+    }
+    special
+    speed {
+      quantity
+      unit
+    }
+    stealth_disadvantage
+    str_minimum
+    throw_range {
+      long
+      normal
+    }
+    tool_category
+    two_handed_damage {
+      damage_dice
+      damage_type {
+        index
+        name
+        url
+      }
+    }
+    url
+    vehicle_category
+    weapon_category
+    weapon_range
+    weight
   }
+}
 `;
---
+++ src/components/equipmentsQuery.ts
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const QUERY_EQUIPMENTS = gql`
+  query getEquipments {
+    equipments(limit: 1000) {
+      __typename
+      index
+      weight
+      name
+      quantity
+      capacity
+      category_range
+      gear_category {
+        name
+      }
+      desc
+      special
+      cost {
+        quantity
+        unit
+        __typename
+      }
+      damage {
+        damage_dice
+        damage_type {
+          name
+          __typename
+        }
+      }
+    }
+  }
+`;
