6b088829e2ca40c3bddcc38b7cdf8b93112e2e87
Jean-Michel Laforge <thejmguitars@gmail.com>
2022-04-13 13:16:31 -0400

    fix(Tools): :lipstick: fixed converter layout

--- src/pages/tools/currency-converter/CurrencyConverter.tsx
+++ src/pages/tools/currency-converter/CurrencyConverter.tsx
@@ -1,6 +1,12 @@
 import {
   Box,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
   Paper,
+  Select,
+  SelectChangeEvent,
   Table,
   TableBody,
   TableCell,
@@ -14,9 +20,11 @@ import {
 import React, { FunctionComponent, useCallback, useState } from "react";
 import { Currency } from "../../../types/gqlCurrency";
 import CurrencyIcon from "../../../components/CurrencyIcon";
-
+import { useDeviceSelectors } from "react-device-detect";
 
 const CurrencyConverter: FunctionComponent = () => {
+  const [{ isMobile }] = useDeviceSelectors(window.navigator.userAgent);
+
   const [initialCurrency, setInitialCurrency] = useState<Currency>(
     Currency.GOLD
   );
@@ -27,6 +35,10 @@ const CurrencyConverter: FunctionComponent = () => {
     newAlignment: Currency
   ) => {
     setInitialCurrency(newAlignment);
+  };
+
+  const handleChange2 = (event: SelectChangeEvent) => {
+    setInitialCurrency(event.target.value as Currency);
   };
 
   const getConvertedValue = useCallback(
@@ -127,80 +139,114 @@ const CurrencyConverter: FunctionComponent = () => {
 
   return (
     <Box
-      component="form"
       sx={{
-        "& .MuiTextField-root": { m: 1, width: "25ch" },
+        display: "flex",
+        mb: "2rem",
       }}
-      noValidate
-      autoComplete="off"
     >
-      <TextField
-        id="outlined-number"
-        label="Number"
-        type="number"
-        value={value}
-        onChange={(e) => setValue(Number(e.target.value))}
-      />
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={12} lg={9}>
+          <Paper variant="outlined" sx={{ padding: 2 }}>
+            <FormControl>
+              <TextField
+                id="outlined-number"
+                label="Number"
+                type="number"
+                value={value}
+                onChange={(e) => setValue(Number(e.target.value))}
+              />
+            </FormControl>
 
-      <ToggleButtonGroup
-        size="small"
-        color="primary"
-        value={initialCurrency}
-        exclusive
-        onChange={handleChange}
-      >
-        {Object.keys(Currency).map((key) => (
-          <ToggleButton value={key as Currency}>
-            <div style={{ display: "flex", alignItems: "center" }}>
-              {key} <CurrencyIcon currency={key as Currency} />{" "}
-            </div>
-          </ToggleButton>
-        ))}
-      </ToggleButtonGroup>
-
-      <TableContainer component={Paper}>
-        <Table size="small" aria-label="a dense table">
-          <TableBody>
-            {Object.keys(Currency).map((key) => (
-              <TableRow>
-                <TableCell>
-                  <div style={{ display: "flex", alignItems: "center" }}>
-                    <span
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        marginRight: ".5rem",
-                      }}
-                    >
-                      {getConvertedValue(key as Currency)}{" "}
-                    </span>
-                    <span
-                      style={{
-                        display: "flex",
-                        alignItems: "center",
-                        marginRight: ".5rem",
-                      }}
-                    >
-                      <CurrencyIcon currency={key as Currency} />
-                    </span>
-                    <Typography
-                      sx={{
-                        display: "flex",
-                        alignItems: "center",
-                        fontStyle: "italic",
-                        fontSize: "xx-small",
-                        fontWeight: 100,
-                      }}
-                    >
-                      ({key})
-                    </Typography>
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+            {isMobile ? (
+              <FormControl fullWidth>
+                <InputLabel id="demo-simple-select-label">
+                  Initial Currency
+                </InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  label="Initial Currency"
+                  value={initialCurrency}
+                  onChange={handleChange2}
+                >
+                  {Object.keys(Currency).map((key) => (
+                    <MenuItem value={key as Currency}>
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        {key} <CurrencyIcon currency={key as Currency} />{" "}
+                      </div>
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+            ) : (
+              <FormControl fullWidth>
+                <ToggleButtonGroup
+                  size="small"
+                  color="primary"
+                  value={initialCurrency}
+                  exclusive
+                  onChange={handleChange}
+                >
+                  {Object.keys(Currency).map((key) => (
+                    <ToggleButton value={key as Currency}>
+                      <div style={{ display: "flex", alignItems: "center" }}>
+                        {key} <CurrencyIcon currency={key as Currency} />{" "}
+                      </div>
+                    </ToggleButton>
+                  ))}
+                </ToggleButtonGroup>
+              </FormControl>
+            )}
+          </Paper>
+        </Grid>
+        <Grid item xs={12} md={12} lg={3}>
+          <Paper elevation={3}>
+            <TableContainer>
+              <Table size="small" aria-label="a dense table">
+                <TableBody>
+                  {Object.keys(Currency).map((key) => (
+                    <TableRow>
+                      <TableCell>
+                        <div style={{ display: "flex", alignItems: "center" }}>
+                          <span
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              marginRight: ".5rem",
+                            }}
+                          >
+                            {getConvertedValue(key as Currency)}{" "}
+                          </span>
+                          <span
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              marginRight: ".5rem",
+                            }}
+                          >
+                            <CurrencyIcon currency={key as Currency} />
+                          </span>
+                          <Typography
+                            sx={{
+                              display: "flex",
+                              alignItems: "center",
+                              fontStyle: "italic",
+                              fontSize: "xx-small",
+                              fontWeight: 100,
+                            }}
+                          >
+                            ({key})
+                          </Typography>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </Grid>
+      </Grid>
     </Box>
   );
 };
